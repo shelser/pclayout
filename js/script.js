e6a298f54587d974fd813508e076aa28e87e6916
shelser <shelserg@gmail.com>
2025-11-09 15:41:06 +0300

    added new files

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const contents = document.querySelectorAll('.program-line__content');
+
+contents.forEach((item) => {
+    const title = item.querySelector('.program-line__title');
+    const descr = item.querySelector('.program-line__descr');
+  
+   
+    
+    title.addEventListener('click', () => {
+        contents.forEach((content) => {
+            const contentDescr = content.querySelector('.program-line__descr');
+            contentDescr.classList.remove('active');
+        });
+        descr.classList.toggle('active');
+    })
+})
