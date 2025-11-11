4e393726e94d94c1cb5fec91e494220714cb82e1
shelser <shelserg@gmail.com>
2025-11-11 09:26:04 +0300

    added new files

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,39 @@
 
-
-const modalBtn = document.querySelector('.modal__button');
+const modalBtn = document.querySelector('.course__button');
 const modal = document.querySelector('.modal');
+const modalInner = document.querySelector('.modal__inner');
+
+
+const cross = document.createElement('button');
+cross.classList.add('cross');
+cross.textContent = 'x';
+modalInner.style.position = 'relative';
+cross.style.cssText = `
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    font-size: 15px;
+    top: 15px;
+    right: 15px;
+`;
+modalInner.appendChild(cross);
 
 
 modalBtn.addEventListener('click', () => {
     modal.style.display = 'flex';
-})
+});
 
 modal.addEventListener('click', (e) => {
     const modalContent = e.target.closest('.modal__inner')
     if (!modalContent) {
         modal.style.display = '';
     }
+});
+
+const crossBtn = document.querySelector('.cross');
+
+crossBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.style.display = '';
 })
+
